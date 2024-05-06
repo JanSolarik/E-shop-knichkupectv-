e70bda8a562dfcb0610bf2c3bd6cbdd2ed8b9c1f
Letaclo <sillyhonzix@gmail.com>
2024-05-06 18:52:55 +0200

    Začátek

---
+++ src/main/java/cz/solarik/knihkupectvi/KnihkupectviApplication.java
@@ -0,0 +1,13 @@
+package cz.solarik.knihkupectvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnihkupectviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnihkupectviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/solarik/knihkupectvi/KnihkupectviApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.solarik.knihkupectvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnihkupectviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
