a38ea25c44dde127b3fc32f7692bfff1897c7e81
Letaclo <sillyhonzix@gmail.com>
2024-05-27 21:17:06 +0200

    table create

---
+++ src/main/java/cz/solarik/knihkupectvi/LoginSave.java
@@ -0,0 +1,4 @@
+package cz.solarik.knihkupectvi;
+import java.sql.*;
+public class LoginSave {
+}
---
+++ src/main/java/cz/solarik/knihkupectvi/User.java
@@ -0,0 +1,11 @@
+package cz.solarik.knihkupectvi;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class User {
+    @Id
+    private Long id;
+}
+
